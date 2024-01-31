9928db235f2526d697630e0cca0df96ff06989d9
Daniel Camargo <danidcp098@gmail.com>
2024-01-31 09:58:53 -0300

    studinRoutersinAngular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfessoresComponent } from './professores/professores.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { DashboardComponent } from './dashboard/Dashboard.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'Dashboard', pathMatch:'full'},
+  {path: 'Dashboard' , component: DashboardComponent},
+  {path: 'alunos' , component: AlunosComponent},
+  {path: 'perfil' , component: PerfilComponent},
+  {path: 'professores' , component: ProfessoresComponent},
+];  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
