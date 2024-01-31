99a363580c1389e37f6fdb81861563dd938b7bd4
Daniel Camargo <danidcp098@gmail.com>
2024-01-30 22:34:41 -0300

    EstudoAngular

---
+++ src/app/alunos/alunos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alunos',
+  templateUrl: './alunos.component.html',
+  styleUrls: ['./alunos.component.css']
+})
+export class AlunosComponent implements OnInit {
+  titulo = "Alunos";
+  public alunos = [ 
+  'Emily',
+  'Eduarda',
+  'Elaine',
+  'Deriky',
+  'Mark',
+  'Kadiama',
+];
+   
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { ProfessoresComponent } from './professores/professores.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [					
+    AppComponent,
+      AlunosComponent,
+      ProfessoresComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
